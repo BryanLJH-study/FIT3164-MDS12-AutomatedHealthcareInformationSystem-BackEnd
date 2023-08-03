a5c1c68e3771bc1a7eeb0f204b7f352a1a296c92
Bryan Lean <bryanleanjun.study@gmail.com>
2023-08-03 16:51:03 +0800

    Added basic Prisma database functionallity

---
+++ prisma/migrations/20230803084252_bare_employee_and_patient_entities/migration.sql
@@ -0,0 +1,42 @@
+-- CreateTable
+CREATE TABLE `Employee` (
+    `employeeId` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `hash` VARCHAR(191) NOT NULL,
+    `IC` VARCHAR(191) NOT NULL,
+    `firstName` VARCHAR(191) NOT NULL,
+    `lastName` VARCHAR(191) NOT NULL,
+    `dob` DATETIME(3) NOT NULL,
+    `gender` VARCHAR(191) NOT NULL,
+    `nationality` VARCHAR(191) NOT NULL,
+    `phoneNo` VARCHAR(191) NOT NULL,
+    `emergencyNo` VARCHAR(191) NULL,
+    `emergencyRemarks` VARCHAR(191) NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `specialty` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `Employee_email_key`(`email`),
+    PRIMARY KEY (`employeeId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Patient` (
+    `patientId` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `IC` VARCHAR(191) NOT NULL,
+    `firstName` VARCHAR(191) NOT NULL,
+    `lastName` VARCHAR(191) NOT NULL,
+    `dob` DATETIME(3) NOT NULL,
+    `gender` VARCHAR(191) NOT NULL,
+    `nationality` VARCHAR(191) NOT NULL,
+    `phoneNo` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `emergencyNo` VARCHAR(191) NULL,
+    `emergencyRemarks` VARCHAR(191) NULL,
+    `remarks` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`patientId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { EmployeeModule } from './employee/employee.module';
 import { PatientModule } from './patient/patient.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, EmployeeModule, PatientModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule, 
+    EmployeeModule, 
+    PatientModule, 
+    PrismaModule,
+  ],
   controllers: [],
   providers: [],
 })
+
 export class AppModule {}
