6030a5eafc47df0cacc9dcce733ae56bb8e49a1c
Bryan Lean <blea0003@student.monash.edu>
2023-08-06 04:09:09 +0800

    Cleared migration history  and added follow up appointment relationships.

--- prisma/migrations/20230805180606_fixed_completed_column_capitalization/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `Completed` on the `Appointment` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `Appointment` DROP COLUMN `Completed`,
-    ADD COLUMN `completed` BOOLEAN NOT NULL DEFAULT false;
--- prisma/migrations/20230805200127_basic_patient_employee_appointment_tables/migration.sql
+++ prisma/migrations/20230805200127_basic_patient_employee_appointment_tables/migration.sql
@@ -40,3 +40,28 @@ CREATE TABLE `Patient` (
 
     PRIMARY KEY (`patientId`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Appointment` (
+    `appointmentId` INTEGER NOT NULL AUTO_INCREMENT,
+    `registrationDateTime` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `patientId` INTEGER NOT NULL,
+    `employeeId` INTEGER NOT NULL,
+    `appointmentDateTime` DATETIME(3) NOT NULL,
+    `reason` VARCHAR(191) NULL,
+    `remarks` VARCHAR(191) NULL,
+    `completed` BOOLEAN NOT NULL DEFAULT false,
+    `previousAppointmentId` INTEGER NULL,
+
+    UNIQUE INDEX `Appointment_previousAppointmentId_key`(`previousAppointmentId`),
+    PRIMARY KEY (`appointmentId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Appointment` ADD CONSTRAINT `Appointment_patientId_fkey` FOREIGN KEY (`patientId`) REFERENCES `Patient`(`patientId`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Appointment` ADD CONSTRAINT `Appointment_employeeId_fkey` FOREIGN KEY (`employeeId`) REFERENCES `Employee`(`employeeId`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Appointment` ADD CONSTRAINT `Appointment_previousAppointmentId_fkey` FOREIGN KEY (`previousAppointmentId`) REFERENCES `Appointment`(`appointmentId`) ON DELETE SET NULL ON UPDATE CASCADE;
