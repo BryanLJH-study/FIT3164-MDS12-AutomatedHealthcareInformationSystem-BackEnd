3f4e421e028c97e0e6c06b54f7bae4c3624cce07
Bryan Lean <blea0003@student.monash.edu>
2023-08-09 16:55:39 +0800

    Added getDoctors function

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -3,12 +3,30 @@ import { Employee } from '@prisma/client';
 import { Request } from 'express';
 import { GetEmployeeInfo } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @UseGuards(JwtGuard)
 @Controller('employees')
 export class EmployeeController {
+    constructor(private prisma: PrismaService) {}
+
     @Get('myinfo')
     getMyInfo(@GetEmployeeInfo() employee: Employee) {
         return employee;
+    }
+
+    @Get('doctors')
+    getDoctors() {
+        return this.prisma.employee.findMany({
+            where: {
+                title: 'Doctor',
+            },
+            select: {
+                employeeId: true,
+                firstName: true,
+                lastName: true,
+                specialty: true,
+            }
+        })
     }
 }
