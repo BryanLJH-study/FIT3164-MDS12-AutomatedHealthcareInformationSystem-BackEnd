f1402491c38968958270682e9166a95b9aaeab34
Bryan Lean <blea0003@student.monash.edu>
2023-08-26 21:48:55 +0800

    Outlined general testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -31,5 +31,111 @@ describe('App e2e', () => {
     app.close()
   });
   
-  it.todo('should pass');
+  describe('Auth', () => {
+    describe('Sign Up', () => {
+      it('should sign up doctor', () => {
+      
+      });
+
+      it('should sign up nurse', () => {
+      
+      });
+    });
+
+    describe('Sign In', () => {
+      it('should sign in', () => {
+      
+      });
+    });
+
+    describe('Get My Info', () => {
+      
+    });
+  });
+
+  describe('Patient', () => {
+    it('should get no patients with "getAllPatients', () => {
+      
+    });
+
+
+    describe('Add Patient', () => {
+      
+    });
+
+    describe('Edit Patient Data', () => {
+      
+    });
+
+    it('should get no patients with "getAllPatients', () => {
+      
+    });
+
+    it('should get patients with "getPatientById', () => {
+      
+    });
+  });
+
+  describe('Employee', () => {
+    it('Get All Doctors', () => {
+      
+    });
+  });
+
+  describe('Appointment', () => {
+    it('should get no appointments with "getAllAppointments', () => {
+      
+    });
+
+    describe('Add Appointment', () => {
+      
+    });
+
+    describe('Edit Appointment Data', () => {
+      
+    });
+
+    describe('Delete Appointment', () => {
+      
+    });
+
+    it('should get appointments with "getAllAppointments', () => {
+      
+    });
+
+    it('should get appointments with "getAppointmentsByPatientId"', () => {
+      
+    });
+
+    it('should get appointments with "getAppointmentsByEmployeeId"', () => {
+      
+    });
+  });
+
+  describe('Diagnosis', () => {
+    it('should get no diagnoses with "getAllDiagnoses', () => {
+      
+    });
+
+    it('should get appointments with "getDiagnosesByPatientId"', () => {
+      
+    });
+
+    describe('Add Diagnosis', () => {
+      
+    });
+
+    describe('Edit Diagnosis Data', () => {
+      
+    });
+
+    describe('Delete Diagnosis', () => {
+      
+    });
+
+    it('should get diagnoses with "getAllDiagnoses', () => {
+      
+    });
+  });
+
 })
