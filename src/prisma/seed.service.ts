36468c5277627df2b8382d78b91ca9501a08f74b
Chew Yu Han <yuhanchew@gmail.com>
2023-09-12 00:02:31 +0800

    modfy prisma seed

--- src/prisma/seed.service.ts
+++ src/prisma/seed.service.ts
@@ -181,6 +181,7 @@ export class SeedService {
                 email: "IanOong@gmail.com",
                 emergencyNo: "5552368",
                 emergencyRemarks: "According to all known laws of aviation, a bee should not be able to fly...",
+                remarks: "I'm allergic to bees",
             }
         })
 
